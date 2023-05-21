1225da6a7b962a4fa1c088d49f107cdbb1fd7e37
Nathan Leiby <nathanleiby@gmail.com>
2023-05-21 01:38:16 -0700

    fix: gh pages deploy.. added base for vite build
    https://stackoverflow.com/a/71032079/950683

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from "vite";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/sup/", // needed for GH Pages deploy
   // server: {
   //   port: 3000,
   // },
